472b664746073b6791cbf10a86d5d1298b0b8398
secondman <secondman@jetstreamlabs.com>
2023-02-02 14:36:40 -0700

    tests: added tests back after configuration
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

---
+++ tests/Feature/Fixers/BracesFixerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+it('fixes the code', function () {
+  [$statusCode, $output] = run('default', [
+    'path' => base_path('tests/Fixtures/fixers/braces.php'),
+    '--preset' => 'laravel',
+  ]);
+
+  expect($statusCode)->toBe(1)
+    ->and($output)
+    ->toContain('  ⨯')
+    ->toContain(
+      <<<'EOF'
+  -$a = function ()
+  -{
+  +$a = function () {
+       // ..
+   };
+EOF,
+    )->toContain(
+      <<<'EOF'
+  -new class {
+  +new class
+  +{
+       // ..
+   };
+EOF,
+    )->toContain(
+      <<<'EOF'
+  -new class extends stdClass {
+  +new class extends stdClass
+  +{
+       // ..
+   };
+EOF,
+    );
+});
