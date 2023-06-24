9b6bb5530ca3f1ac5ebd315fd8c38662b59bcdb6
secondman <secondman@jetstreamlabs.com>
2023-06-24 10:50:50 -0700

    refactor: update deps and versions
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- config/app.php
+++ config/app.php
@@ -1,63 +1,45 @@
 <?php
 
 return [
-
-  /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
-
+  /**
+   * APPLICATION NAME
+   *
+   * This value is the name of your application. This value is used when the
+   * framework needs to place the application's name in a notification or
+   * any other location as required by the application or its packages.
+   */
   'name' => 'Pinte',
 
-  /*
-    |--------------------------------------------------------------------------
-    | Application Version
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "version" your application is currently running
-    | in. You may want to follow the "Semantic Versioning" - Given a version
-    | number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
-    |
-    */
+  /**
+   * APPLICATION VERSION
+   *
+   * This value determines the "version" your application is currently running
+   * in. You may want to follow the "Semantic Versioning" - Given a version
+   * number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
+   */
+  'version' => '1.0.7',
 
-  'version' => '1.0.5',
-
-  /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. This can be overridden using
-    | the global command line "--env" option when calling commands.
-    |
-    */
-
+  /**
+   * APPLICATION ENVIRONMENT
+   *
+   * This value determines the "environment" your application is currently
+   * running in. This may determine how you prefer to configure various
+   * services the application utilizes. This can be overridden using
+   * the global command line "--env" option when calling commands.
+   */
   'env' => 'development',
 
-  /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
+  /**
+   * AUTOLOADED SERVICE PROVIDERS
+   *
+   * The service providers listed here will be automatically loaded on the
+   * request to your application. Feel free to add your own services to
+   * this array to grant expanded functionality to your applications.
+   */
   'providers' => [
     App\Providers\ActionsServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\CommandsServiceProvider::class,
     App\Providers\RepositoriesServiceProvider::class,
   ],
-
 ];
